5cf308ccf3bd741994206fc6b48c9dc8b5a0fa4b
Klim Orlov <klimetzc@yandex.ru>
2022-04-01 20:00:08 +0300

    fix: code review #1

--- src/validate.js
+++ src/validate.js
@@ -70,7 +70,7 @@ enableValidation({
   formSelector: ".popup__form",
   inputSelector: ".popup__input",
   submitButtonSelector: ".popup__submit",
-  inactiveButtonClass: "popup__button_disabled",
+  inactiveButtonClass: "popup__submit_inactive",
   inputErrorClass: "popup__input_type_error",
   errorClass: "popup__error_visible",
 });
